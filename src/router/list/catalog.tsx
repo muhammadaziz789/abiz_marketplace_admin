445837b0ce86d14a5f3c0c6eb0ad3644ff6fca75
muhammadaziz <aziz.nigmatjonov7@gmail.com>
2024-03-15 16:19:51 +0500

    fixed

--- src/router/list/catalog.tsx
+++ src/router/list/catalog.tsx
@@ -1,10 +1,1 @@
-export const catalogList = [
-  {
-    parent: '',
-    title: "test",
-    path: "test",
-    icon: 'catalog',
-    sidebar: true,
-    element: "test",
-  },
-];
+export const catalogList = [];
